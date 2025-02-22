033fbc7de77874a1e55635d27c53cf3ca2a05672
Kapil <kanurkapil@gmail.com>
2025-02-22 19:40:58 +0530

    Added lib to create topic during application startup

---
+++ src/main/java/com/kk/x_stream_simulator/configs/KafkaTopicConfiguration.java
@@ -0,0 +1,26 @@
+package com.kk.x_stream_simulator.configs;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfiguration {
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put("bootstrap.servers", "localhost:9092");
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic createTweetTopic() {
+        return new NewTopic("tweets", 3, (short) 1);
+    }
+
+}
