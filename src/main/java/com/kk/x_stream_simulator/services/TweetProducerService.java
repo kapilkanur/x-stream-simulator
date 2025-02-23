334c45590fc6c47ce101af2a7dc9099b1e94ec20
Kapil <kanurkapil@gmail.com>
2025-02-23 18:19:39 +0530

    Added error handling for the producer

--- src/main/java/com/kk/x_stream_simulator/services/TweetProducerService.java
+++ src/main/java/com/kk/x_stream_simulator/services/TweetProducerService.java
@@ -2,8 +2,12 @@ package com.kk.x_stream_simulator.services;
 
 import com.kk.x_stream_simulator.models.Tweet;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @Slf4j
@@ -17,6 +21,17 @@ public class TweetProducerService {
 
     public void sendTweet(Tweet tweet) {
         log.info("Sending tweet: {}", tweet);
-        kafkaTemplate.send("tweets", tweet.getId(), tweet);
+        CompletableFuture<SendResult<String, Tweet>> future =
+                kafkaTemplate.send("tweet-stream", tweet.getId(), tweet);
+
+        future.whenComplete((result, ex) -> {
+            if (ex != null) {
+                log.error("Failed to send tweet: {} due to {}", tweet, ex.getMessage());
+            } else {
+                RecordMetadata metadata = result.getRecordMetadata();
+                log.info("Tweet sent successfully to topic {} at partition {} with offset {}",
+                        metadata.topic(), metadata.partition(), metadata.offset());
+            }
+        });
     }
 }
